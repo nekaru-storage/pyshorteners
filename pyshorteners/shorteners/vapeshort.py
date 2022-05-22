b0ada04c53c3a978b2ce4d7a860c291c0faa1670
vapeluckymanbot <104741743+vapeluckymanbot@users.noreply.github.com>
2022-05-22 10:46:39 +0530

    Adding vapeshort

---
+++ pyshorteners/shorteners/vapeshort.py
@@ -0,0 +1,16 @@
+from pyshorteners.base import BaseShortener
+
+class Shortener(BaseShortener):
+    """
+    Docstring
+    """
+    api_url = 'https://short.vapeluckyman.xyz/api'
+
+    def short(self, url):
+        pass
+
+    def expand(self, url):
+        pass
+
+    def custom_method(self):
+        pass
